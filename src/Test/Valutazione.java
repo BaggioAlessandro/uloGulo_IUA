e6ef73e0dfbdd020ee6456c9e8199ebf592d9251
BaggioAlessandro <alebaggio91@hotmail.it>
2014-11-14 11:15:38 +0100

    test

--- src/Test/Valutazione.java
+++ src/Test/Valutazione.java
@@ -8,35 +8,49 @@ public class Valutazione {
 	public static void main(String[] args) throws Exception {
 		Instances rating_test = Roba_utile.load("src/Dati/Test/relevant_test.arff");
 		
-		Instances submission = Roba_utile.load("src/Dati/Tesr/sub.arff");
+		Instances submission = Roba_utile.load("src/Dati/Test/new_submission_sample.arff");
 		
 		float media = 0;
-		float tot = 0;
+		
 		for(int i = 0; i < submission.numInstances(); i++){
-			int count = 0;
+			double tot = 0;
+			float count = 0;
 			Integer[] ok = new Integer[5];
+			
+			for(int j = 4; j >= 0; j--){
+				ok[j] = 0;
+			}
+			
 			for(int j = 0; j < rating_test.numInstances(); j++){
-				if(rating_test.instance(j).value(2) == submission.instance(i).value(1)){
-					count++;
-					ok[0] = 1;
-				}else if(rating_test.instance(j).value(2) == submission.instance(i).value(2)){
-					count++;
-					ok[1] = 1;
-				}else if(rating_test.instance(j).value(2) == submission.instance(i).value(3)){
-					count++;
-					ok[2] = 1;
-				}else if(rating_test.instance(j).value(2) == submission.instance(i).value(4)){
-					count++;
-					ok[3] = 1;
-				}else if(rating_test.instance(j).value(2) == submission.instance(i).value(5)){
-					count++;
-					ok[4] = 1;
+				if(rating_test.instance(j).value(0) == submission.instance(i).value(0)){
+					if(rating_test.instance(j).value(1) == submission.instance(i).value(1)){
+						System.out.println("1");
+						count++;
+						ok[0] = 1;
+					}else if(rating_test.instance(j).value(1) == submission.instance(i).value(2)){
+						System.out.println("1");
+						count++;
+						ok[1] = 1;
+					}else if(rating_test.instance(j).value(1) == submission.instance(i).value(3)){
+						System.out.println("1");
+						count++;
+						ok[2] = 1;
+					}else if(rating_test.instance(j).value(1) == submission.instance(i).value(4)){
+						System.out.println("1");
+						count++;
+						ok[3] = 1;
+					}else if(rating_test.instance(j).value(1) == submission.instance(i).value(5)){
+						System.out.println("1");
+						count++;
+						ok[4] = 1;
+					}
 				}
+				
 			}
 			
 			for(int j = 4; j >= 0; j--){
 				if(ok[j]!=0){
-					tot += 1.0/(count/j);
+					tot = tot + (count/(j+1));
 					count--;
 				}
 			}
@@ -46,6 +60,7 @@ public class Valutazione {
 		}
 		
 		media = media/submission.numInstances();
+		System.out.println(media);
 		
 	}
 }
