37f8f4f6abb95a9e96de509045307ea2e23220ee
BaggioAlessandro <alebaggio91@hotmail.it>
2014-11-28 20:15:56 +0100

    update

--- src/Test/Relevant.java
+++ src/Test/Relevant.java
@@ -6,7 +6,7 @@ import weka.core.Instances;
 public class Relevant {
 
 	public static void main(String[] args) throws Exception {
-		Instances rating_test = Roba_utile.load("src/Dati/Test/rating_test.arff");
+		Instances rating_test = Roba_utile.load("src/Dati/Test/True/rating_test.arff");
 		
 		
 		for(int i = 0; i < rating_test.numInstances();){
@@ -16,7 +16,7 @@ public class Relevant {
 			else i++;
 		}
 		
-		Roba_utile.save(rating_test, "src/Dati/Test/relevant_test.arff");
+		Roba_utile.save(rating_test, "src/Dati/Test/True/relevant_test.arff");
 		
 		System.out.println("fine");
 		
--- src/Test/Valutazione.java
+++ src/Test/Valutazione.java
@@ -1,17 +1,31 @@
 package Test;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import PreProcessing.Parser_submission;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 import weka.core.Instances;
 import librerie_Aggiunte.Roba_utile;
 
 public class Valutazione {
 
+	static String submission_path = new String("src/Dati/Test/True/res.csv");
+	static String output = new String("src/Dati/Test/True/Temp/temp.arff");
+	
 	public static void main(String[] args) throws Exception {
-		Instances rating_test = Roba_utile.load("src/Dati/Test/relevant_test.arff");
+		parser_submission();
 		
-		Instances submission = Roba_utile.load("src/Dati/Test/submission.arff");
+		Instances rating_test = Roba_utile.load("src/Dati/Test/True/relevant_test.arff");
+		
+		Instances submission = Roba_utile.load(output);
 		
 		float media = 0;
-		
+		int count_non_relevant = 0;
 		for(int i = 0; i < submission.numInstances(); i++){
 			double tot = 0;
 			float count = 0;
@@ -53,16 +67,54 @@ public class Valutazione {
 					count--;
 				}
 			}
-			if(nRelevant!=0)
-				media += tot/5;
-			else
-				System.out.println("fdsfsd");
-			
+			if(nRelevant!=0){
+				media += tot/nRelevant;
+			}else
+				count_non_relevant++;
 		}
 		
 		media = media/submission.numInstances();
 		System.out.println(media);
-		System.out.println("Top popular scored: 0.3143" );
+		System.out.println("utenti senza relevant item sono: "+count_non_relevant );
 		
+	}
+	
+	private static void parser_submission() throws IOException{
+		CSVReader reader = new CSVReader(new FileReader(submission_path ));
+		
+		BufferedWriter br = new BufferedWriter(new FileWriter(output));
+			
+		String new_header = new String("@relation rel\n"+
+										"\n"+
+										"@attribute UserId numeric\n"+
+										"@attribute RecommendedMovieIds1 numeric\n"+
+										"@attribute RecommendedMovieIds2 numeric\n"+
+										"@attribute RecommendedMovieIds3 numeric\n"+
+										"@attribute RecommendedMovieIds4 numeric\n"+
+										"@attribute RecommendedMovieIds5 numeric\n"+
+										"\n"+
+										"@data"+
+										"\n");		
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(new_header);
+		
+		reader.readNext();
+		String[] line = reader.readNext();
+		while(line != null){
+			sb.append(line[0]);
+			sb.append(",");
+			String recommendation = line[1].replace(' ', ',');
+			if (recommendation.lastIndexOf(',') == recommendation.length()-1)
+				recommendation = recommendation.substring(0, recommendation.length()-2);
+			sb.append(recommendation);
+			sb.append("\n");
+			
+			line = reader.readNext();
+		}
+		
+		br.write(sb.toString());
+		br.close();
+		reader.close();
 	}
 }
