fa4d999615690a73a3c7e6f560d4c9c399c75ef3
camilomartinez <marscamand@gmail.com>
2015-01-26 17:20:39 +0100

    Add submission profile evaluation

--- MatLab/evaluate_submission_file.m
+++ MatLab/evaluate_submission_file.m
@@ -1,6 +1,6 @@
 %% Setup
 % Submission to test
-file_to_test = './submissions/thelast+ar.csv';
+file_to_test = './submissions/svd.csv';
 
 %% Test
 % Add functions folder to path to avoid call errors
@@ -9,4 +9,26 @@ addpath(genpath('./functions'));
 expected = load_expected();
 test = load_submission(file_to_test);
 % evaluate using map_at_k
-mapTest = map_at_k_relevant(expected, test, 5)
+map_at_5 = map_at_k_relevant(expected, test, 5)
+
+%% map profile
+mapi = zeros(1,5);
+for i = 1:5
+    mapi(i) = map_at_k_relevant(expected, test(:,[1 i+1]), 1);
+end
+mapacc = zeros(1,5);
+for i = 1:5
+    mapacc(i) = map_at_k_relevant(expected, test, i);
+end
+
+%% Plot
+plot_matrix = [map_at_5 * ones(1,5); mapi; mapacc]';
+
+figure;
+h = plot(plot_matrix);
+h(2).Marker = '*';
+h(3).Marker = 'o';
+legend('map@5','map(i)@1','map@i');
+title(strcat('map profile for ', file_to_test));
+xlabel('i')
+ylabel('map')
--- MatLab/functions/submission_mat2cell.m
+++ MatLab/functions/submission_mat2cell.m
@@ -1,6 +1,6 @@
 function cell_submission = submission_mat2cell( mat_submission )
 %SUBMISSION_MAT2CELL Transform matricial output format to cell array
 %   from a n_usersx6 matrix to a n_users length cell_array
-    cell_submission = arrayfun( @(id) mat_submission(mat_submission(:,1)==id, [2 3 4 5 6]), mat_submission(:,1), 'UniformOutput', 0 );
+    cell_submission = arrayfun( @(id) mat_submission(mat_submission(:,1)==id, [2:end]), mat_submission(:,1), 'UniformOutput', 0 );
 end
 
